4753b2dc17af4c806a668894a7a04c84511b0b4e
Angelo Turco <turcoa3@mcmaster.ca>
2023-11-21 15:41:24 -0500

    message

--- Player.cpp
+++ Player.cpp
@@ -7,6 +7,7 @@ Player::Player(GameMechs* thisGMRef)
     myDir = STOP;
 
     // more actions to be included
+    // playerPos.setObjPos= (5,5,'@');
 }
 
 
@@ -18,15 +19,99 @@ Player::~Player()
 void Player::getPlayerPos(objPos &returnPos)
 {
     // return the reference to the playerPos arrray list
+    // return Pos.setObjPos (playerPos.x, PlayerPos.y, PlayerPos.symbol);
 }
 
 void Player::updatePlayerDir()
 {
-    // PPA3 input processing logic        
+    // PPA3 input processing logic  
+    // not macuilib char
+    // get input from where?
+    // game mech will find out where to get input
+    // it lies within the Gamemechanism* member
+
+
+    // switch (input)
+    // {
+    //     case 'w':
+    //         if (myDir != UP && myDir != DOWN)
+    //             myDir = UP;
+    //         break;
+
+    //     case 'a':
+    //         if (myDir != LEFT && myDir != RIGHT)
+    //             myDir = LEFT;
+    //         break;
+
+    //     case 's':
+    //         if (myDir != UP && myDir != DOWN)
+    //             myDir = DOWN;
+    //         break;
+
+    //     case 'd':
+    //         if (myDir != LEFT && myDir != RIGHT)
+    //             myDir = RIGHT;
+    //         break;
+
+    //     default:
+    //         break;
+        
+
+
+
+    // }      
 }
 
 void Player::movePlayer()
 {
     // PPA3 Finite State Machine logic
+// if (myDir == UP)
+//     {
+//         if (playerPos.y > 0) 
+//         {
+//             playerPos.y--;
+//         }
+
+//         if (playerPos.y==0)
+//         {
+//             playerPos.y=8;
+//         }
+//     }
+//     else if (myDir == DOWN)
+//     {
+//         if (playerPos.y < 9) 
+//         {
+//             playerPos.y++;
+//         }
+//         if (playerPos.y==9)
+//         {
+//             playerPos.y=1;
+//         }
+//     }
+//     else if (myDir == LEFT)
+//     {
+
+//         if (playerPos.x > 0) 
+//         {
+//             playerPos.x--;
+//         }
+//         if (playerPos.x==0)
+//         {
+//             playerPos.x=18;
+//         }
+//     }
+//     else if (myDir == RIGHT)
+//     {
+
+//         if (playerPos.x < 19)
+//         {
+//             playerPos.x++;
+//         }
+//         if (playerPos.x==19)
+//         {
+//             playerPos.x=1;
+//         }
+//     }
+
 }
 
--- Project.cpp
+++ Project.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "MacUILib.h"
 #include "objPos.h"
+// #include "Player.h"
 
 
 using namespace std;
@@ -16,7 +17,7 @@ void DrawScreen(void);
 void LoopDelay(void);
 void CleanUp(void);
 
-
+// Player* playerPtr; 
 
 int main(void)
 {
@@ -42,6 +43,9 @@ void Initialize(void)
     MacUILib_clearScreen();
 
     exitFlag = false;
+
+    
+    // playerPtr = new Player(/* Pass your game mechanics reference here */);
 }
 
 void GetInput(void)
@@ -56,7 +60,65 @@ void RunLogic(void)
 
 void DrawScreen(void)
 {
-    MacUILib_clearScreen();    
+    MacUILib_clearScreen();
+
+
+
+    const int BOARD_WIDTH = 20;
+    const int BOARD_HEIGHT = 10;
+
+    objPos topRow, bottomRow, leftColumn, rightColumn;
+    topRow.setObjPos(0, 0, '#');
+    bottomRow.setObjPos(0, BOARD_HEIGHT - 1, '#');
+    leftColumn.setObjPos(0, 0, '#');
+    rightColumn.setObjPos(BOARD_WIDTH - 1, 0, '#');
+
+    objPos player(5, 5, 'P'); // Player positioned at (5, 5)
+
+    objPos char1(2, 3, 'A');
+    objPos char2(7, 4, 'B');
+    objPos char3(12, 7, 'C');
+
+    std::cout << "Initial board:" << std::endl;
+    for (int i = 0; i < BOARD_WIDTH; ++i) {
+        std::cout << topRow.getSymbol();
+    }
+    std::cout << std::endl;
+
+    for (int i = 1; i < BOARD_HEIGHT - 1; ++i) {
+        std::cout << leftColumn.getSymbol();
+
+        for (int j = 1; j < BOARD_WIDTH - 1; ++j) {
+
+            if (i == player.y && j == player.x) {
+                std::cout << player.getSymbol();
+
+            } else if ((i == char1.y && j == char1.x) || (i == char2.y && j == char2.x) || (i == char3.y && j == char3.x)) {
+
+                if (i == char1.y && j == char1.x) {
+                    std::cout << char1.getSymbol();
+
+                } 
+                else if (i == char2.y && j == char2.x) {
+                    std::cout << char2.getSymbol();
+
+                } 
+                else if (i == char3.y && j == char3.x) {
+                    std::cout << char3.getSymbol();
+                }
+
+            } 
+            else {
+                std::cout << ' ';
+            }
+        }
+        std::cout << rightColumn.getSymbol() << std::endl;
+    }
+
+    for (int i = 0; i < BOARD_WIDTH; ++i) {
+        std::cout << bottomRow.getSymbol();
+    }
+    std::cout << std::endl;
 
 }
 
@@ -69,6 +131,7 @@ void LoopDelay(void)
 void CleanUp(void)
 {
     MacUILib_clearScreen();    
-  
+    // delete playerPtr;
+    // playerPtr = nullptr;
     MacUILib_uninit();
 }
